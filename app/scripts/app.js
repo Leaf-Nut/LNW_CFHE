c044b230a3a2b6a1211d0f3ed126eea892bdff77
leaf-nut <iyeon88@gmail.com>
2016-01-05 18:54:43 +0900

    init

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name cfheApp
+ * @description
+ * # cfheApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('cfheApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cfheApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cfheApp
+ */
+angular.module('cfheApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
